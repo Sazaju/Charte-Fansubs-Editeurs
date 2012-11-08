8bc4649064f53fbae3632e3d8a66221b80c41d4b
Sazaju HITOKAGE <sazaju@gmail.com>
2012-11-08 01:29:47 +0100

    MAJ copyright (plus général).

--- copyright.php
+++ copyright.php
@@ -1,5 +1,10 @@
-<p xmlns:dct="http://purl.org/dc/terms/" xmlns:vcard="http://www.w3.org/2001/vcard-rdf/3.0#">
-	<a rel="license" href="http://creativecommons.org/publicdomain/zero/1.0/"><img src="http://i.creativecommons.org/p/zero/1.0/80x15.png" style="border-style: none;" alt="CC0" /></a>
-	To the extent possible under law, <span resource="[_:publisher]" rel="dct:publisher"><span property="dct:title">Sazaju HITOKAGE</span></span> has waived all copyright and related or neighboring rights to this work.
-	This work is published from: <span property="vcard:Country" datatype="dct:ISO3166" content="FR" about="[_:publisher]">France</span>.
+<p xmlns:dct="http://purl.org/dc/terms/">
+  <a rel="license"
+     href="http://creativecommons.org/publicdomain/zero/1.0/">
+    <img src="http://i.creativecommons.org/p/zero/1.0/80x15.png" style="border-style: none;" alt="CC0" />
+  </a>
+  To the extent possible under law,
+  <span rel="dct:publisher" resource="[_:publisher]">the person who associated CC0</span>
+  with this work has waived all copyright and related or neighboring
+  rights to this work.
 </p>
