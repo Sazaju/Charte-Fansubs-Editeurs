f06e9dad237fff2267f9f17a9c6610c015717ca8
Sazaju HITOKAGE <sazaju@gmail.com>
2013-07-28 02:33:30 +0200

    Update references + minor update of the home page.

--- class/page/WorksPage.php
+++ class/page/WorksPage.php
@@ -13,13 +13,20 @@ class WorksPage extends Page {
 	}
 	
 	public function getContent() {
-		$content = "<p>Cette liste recense les travaux officiels effectués dans le domaine de l'audiovisuel et du fansub.</p>";
+		$content = "<p>Cette liste recense différents articles, travaux de recherche, billets de blog et autres sources particulièrement informatives dans les domaines de l'audiovisuel, du fansub, du droit d'auteur ou de la copie privée.</p>";
 		$works = array(
 			new Work(
-				"Différenciation des pratiques de réception des séries télévisées et mutations du secteur de la diffusion audiovisuelle : analyse critique du scénario de la mort annoncée de la télévision",
-				"Guillaume Blanc",
-				"28 Mars 2012",
-				"http://dumas.ccsd.cnrs.fr/dumas-00683226/",
+				"Article L122-5",
+				"Code de la propriété intellectuelle",
+				"1er Août 2006",
+				"http://www.legifrance.gouv.fr/affichCodeArticle.do;jsessionid=A49BB9856A165E6FB4B9BDE8EA010909.tpdjo13v_3?idArticle=LEGIARTI000006278917&cidTexte=LEGITEXT000006069414&dateTexte=20080116",
+				"fr"
+			),
+			new Work(
+				"Les droits d'auteur pour les nuls",
+				"Maître Eolas",
+				"20 Février 2009",
+				"http://www.maitre-eolas.fr/post/2009/02/20/1321-les-droits-d-auteur-pour-les-nuls",
 				"fr"
 			),
 			new Work(
@@ -28,6 +35,13 @@ class WorksPage extends Page {
 				"Janvier 2011",
 				"http://www.rieti.go.jp/en/publications/summary/11010021.html",
 				"jap"
+			),
+			new Work(
+				"Différenciation des pratiques de réception des séries télévisées et mutations du secteur de la diffusion audiovisuelle : analyse critique du scénario de la mort annoncée de la télévision",
+				"Guillaume Blanc",
+				"28 Mars 2012",
+				"http://dumas.ccsd.cnrs.fr/dumas-00683226/",
+				"fr"
 			),
 		);
 		$content .= "<ul>";
